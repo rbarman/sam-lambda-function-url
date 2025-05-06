ccd09f429566923c76fea42519d3b9ffbb97ed11
Rohan Barman <rohan1barman@gmail.com>
2025-05-05 20:01:47 -0500

    handler returns Response objects + add tests to the handler

--- src/app.py
+++ src/app.py
@@ -1,24 +1,29 @@
 import json
 from aws_lambda_powertools.event_handler import LambdaFunctionUrlResolver
 from aws_lambda_powertools.utilities.typing import LambdaContext
+from aws_lambda_powertools.event_handler.api_gateway import Response
+from http import HTTPStatus
 
 app = LambdaFunctionUrlResolver()
 
-@app.get("/")
-def root():
-
+@app.get("/search")
+def search():
     current_event = app.current_event
     print(f"received event: {current_event}")
 
     my_id = app.current_event.query_string_parameters.get("my_id")
     print(f"received my_id: {my_id}")
+    if not my_id:
+        return Response(status_code=HTTPStatus.BAD_REQUEST, body="Id parameter is required")
 
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "Hello, World!"
-        })
-    }
+    return Response(
+        status_code=HTTPStatus.OK,
+        content_type="application/json",
+        body={
+            "id": my_id,
+        },
+    )
+
 
 def lambda_handler(event: dict, context: LambdaContext) -> dict:
     return app.resolve(event, context)
--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,4 +1,44 @@
 import pytest
+from http import HTTPStatus
+from src import app
+from dataclasses import dataclass
 
-def test_handler():
-    assert True
+@dataclass
+class LambdaContext:
+    function_name: str = "test"
+    memory_limit_in_mb: int = 128
+    invoked_function_arn: str = "arn:aws:lambda:eu-west-1:123456789012:function:test"
+    aws_request_id: str = "da658bd3-2d6f-4e7b-8ec2-937234644fdc"
+
+
+REQUEST_CONTEXT = {
+    "requestContext": {"requestId": "227b78aa-779d-47d4-a48e-ce62120393b8"}, 
+    "http": {
+        "method": "GET",
+    },
+    "stage": "$default",
+}
+
+@pytest.fixture
+def lambda_context() -> LambdaContext:
+    return LambdaContext()
+
+def test_search_no_id(lambda_context: LambdaContext):
+    minimal_event = {
+        "rawPath": "/search",
+        "requestContext": REQUEST_CONTEXT,
+    }
+    ret = app.lambda_handler(minimal_event, lambda_context)
+    assert ret["statusCode"] == HTTPStatus.BAD_REQUEST
+    assert ret["body"] == "Id parameter is required"
+
+def test_search_with_id(lambda_context: LambdaContext):
+    minimal_event = {
+        "rawPath": "/search",
+        "requestContext": REQUEST_CONTEXT,
+        "queryStringParameters": {
+            "my_id": "123"
+        }
+    }
+    ret = app.lambda_handler(minimal_event, lambda_context)
+    assert ret["statusCode"] == HTTPStatus.OK
